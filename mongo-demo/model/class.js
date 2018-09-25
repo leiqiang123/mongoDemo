003832b8eb6bfb4624d5bd00ffd6cf9ca7678f53
leiqiang123 <1229949407@qq.com>
2018-09-25 21:12:53 +0800

    一个mongo数据库的小demo

---
+++ mongo-demo/model/class.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+const googleClass = new mongoose.Schema({
+    name:String,
+    age:Number,
+    desc:String
+})
+
+const googleModel = mongoose.model('class',googleClass,'class')
+
+module.exports = googleModel
